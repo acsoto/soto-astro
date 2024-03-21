ea33727c577687417004af4069d7b38958820315
soto <59144459+acsoto@users.noreply.github.com>
2024-03-22 01:42:39 +0800

    fix: build bug and rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,15 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-  const posts = await getCollection("posts");
+export async function GET() {
+  const posts = await getCollection("blog");
   return rss({
     title: 'SOTO',
     description: 'Portfolio of ZZH',
-    site: zzhgo.com,
+    site: 'https://www.zzhgo.com',
     items: posts.map((post) => ({
       title: post.data.title,
-      date: post.data.date,
+      pubDate: post.data.date,
       link: `/blog/${post.slug}/`,
     })),
     customData: `<language>en-us</language>`,
