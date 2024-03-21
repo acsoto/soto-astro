1752904f32eee83dddadf83d086d06902f292d3e
soto <59144459+acsoto@users.noreply.github.com>
2024-03-22 00:19:58 +0800

    feat: add typed text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [preact(), tailwind(), mdx()],
   image: {
     remotePatterns: [{ protocol: "https" }],
   }
