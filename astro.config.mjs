3381c20d63e066bbd1e412835b7f1a260ae59c07
soto <59144459+acsoto@users.noreply.github.com>
2024-03-22 15:13:27 +0800

    feat: add Astro Vercel analytics adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), mdx()],
   image: {
     remotePatterns: [{ protocol: "https" }],
-  }
+  },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
