c48b27093af7fa2d07d97430cc30c2495214406d
soto <59144459+acsoto@users.noreply.github.com>
2024-08-28 23:44:55 +0800

    feat: add rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
@@ -11,6 +14,9 @@ export async function GET() {
       title: post.data.title,
       pubDate: post.data.date,
       link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
     customData: `<language>en-us</language>`,
   })
