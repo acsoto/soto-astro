aa910d3f85652e1e4e96986bef5cdf59e1ab5a6f
SOTO <z@mcac.cc>
2025-04-02 23:09:08 +0800

    Refactor styles and update Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,38 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from '@astrojs/vercel/static';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.zzhgo.com',
-    integrations: [react(), mdx(), tailwind({
-        applyBaseStyles: false,
-    }),],
-    image: {
-        domains: ["img.atksoto.com"],
-    },
-    output: 'static',
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
-    markdown: {
-        shikiConfig: {
-            // https://shiki.style/themes
-            theme: 'material-theme-darker',
-        },
-    },
-    prefetch: {
-        prefetchAll: true,
-    }
+  site: 'https://www.zzhgo.com',
+
+  integrations: [react(), mdx(),],
+
+  image: {
+      domains: ["img.atksoto.com"],
+  },
+
+  output: 'static',
+
+  adapter: vercel({
+      webAnalytics: { enabled: true }
+  }),
+
+  markdown: {
+      shikiConfig: {
+          // https://shiki.style/themes
+          theme: 'material-theme-darker',
+      },
+  },
+
+  prefetch: {
+      prefetchAll: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
